9381520fcacbb4ad5ae8a91232081bfcb329d375
Saurabh Gangule <saurabhgangule10@gmail.com>
2024-05-16 21:04:17 +0530

    Hotfix: movie page issue

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import {  useState } from 'react'
+import { useState } from 'react'
 import useSWR from 'swr'
 import FilmCasts from '../../components/FilmCasts'
 import FilmGenres from '../../components/FilmGenres'
@@ -12,53 +12,63 @@ import FilmResources from '../../components/FilmResources'
 import FilmSynopsis from '../../components/FilmSynopsis'
 import Loading from '../../components/Loading'
 import SearchBar from '../../components/SearchBar'
-import { fetcher, pathToSearchMovie, renderResults, sliceArray } from '../../utils'
-import YouTube from "react-youtube";
+import {
+  fetcher,
+  pathToSearchMovie,
+  renderResults,
+  sliceArray,
+} from '../../utils'
+import YouTube from 'react-youtube'
 
 import TorrentList from '../../Common/TorrentList'
-import CardNormal from "../../components/CardNormal"
+import CardNormal from '../../components/CardNormal'
 import Heading from '../../components/Heading'
 
 export default function Movie() {
   const router = useRouter()
   const { id } = router?.query
   const { data: movie, error: movieError } = useSWR(`/api/movie/${id}`, fetcher)
-  const[isTorrent,setisTorrent]=useState(false)
-const handleClose =()=>{
-  setisTorrent(true)
-}
-  if (typeof (window) !== undefined) {
-    console?.log("movie", movie)
+  const [isTorrent, setisTorrent] = useState(false)
+  const handleClose = () => {
+    setisTorrent(true)
   }
+
   if (movieError) return <div>{movieError}</div>
   if (!movie) return <div>{movieError}</div>
 
   const opts = {
-    height: "500px",
-    width: "100%",
-    border: "8px",
+    height: '500px',
+    width: '100%',
+    border: '8px',
     playerVars: {
       autoplay: 0,
     },
-  };
-
+  }
 
   return (
     <>
-   
       <Head>
         <title>{movie?.detail?.title} | Freemoviesfull.xyz</title>
         <meta name='title' content={movie?.detail?.title} />
-        <meta name='description' content={`${data?.detail?.overview.slice(0, 160)}...`} />
+        <meta
+          name='description'
+          content={`${movie?.detail?.overview.slice(0, 160)}...`}
+        />
       </Head>
       <SearchBar
         placeholder='Search for movies'
         searchPath={pathToSearchMovie}
       />
-     
+
       {movie ? (
         <div>
-          {isTorrent?<TorrentList setisTorrent={setisTorrent} runtime={movie?.imdb?.imdb?.runtime} torrent_list={movie?.imdb?.trb_posts}/>:null}
+          {isTorrent ? (
+            <TorrentList
+              setisTorrent={setisTorrent}
+              runtime={movie?.imdb?.imdb?.runtime}
+              torrent_list={movie?.imdb?.trb_posts}
+            />
+          ) : null}
           <section className='flex flex-col sm:mx-8 md:mx-0 md:flex-row md:items-start lg:justify-center'>
             <FilmImage
               src={movie?.detail?.poster_path}
@@ -86,35 +96,42 @@ const handleClose =()=>{
                 handleClose={handleClose}
               />
             </section>
-
           </section>
-          {movie?.imdb?.imdb?.video_list[0]?.key ? <div className='text-gray-300'>
-           
-            <YouTube videoId={movie?.imdb?.imdb?.video_list[0]?.key} style={{ borderRadius: "1px" }}
-              opts={opts} />
-          </div>
-            : null}
+          {movie?.imdb?.imdb?.video_list[0]?.key ? (
+            <div className='text-gray-300'>
+              <YouTube
+                videoId={movie?.imdb?.imdb?.video_list[0]?.key}
+                style={{ borderRadius: '1px' }}
+                opts={opts}
+              />
+            </div>
+          ) : null}
           <br />
-       {movie?.credits?.cast?   <FilmCasts casts={movie?.credits?.cast} />:null}
+          {movie?.credits?.cast ? (
+            <FilmCasts casts={movie?.credits?.cast} />
+          ) : null}
 
-       <section
-          className='w-full overflow-hidden md:mb-10 lg:overflow-visible'>
-          <Heading
-            title={"You may like also"}
-            isHomePage={true}
-            isTrending={true}
-            media_type={false}
-          />
-          <section
-            className={
-             'h-scroll relative flex gap-x-4 overflow-x-scroll  sm:gap-x-10 2xs:mt-2'   }>
-            {renderResults(
-              sliceArray(movie?.detail?.recommendations?.results || [], movie?.detail?.recommendations?.results.length),
-              CardNormal,
-              "movie"
-            )}
+          <section className='w-full overflow-hidden md:mb-10 lg:overflow-visible'>
+            <Heading
+              title={'You may like also'}
+              isHomePage={true}
+              isTrending={true}
+              media_type={false}
+            />
+            <section
+              className={
+                'h-scroll relative flex gap-x-4 overflow-x-scroll  2xs:mt-2 sm:gap-x-10'
+              }>
+              {renderResults(
+                sliceArray(
+                  movie?.detail?.recommendations?.results || [],
+                  movie?.detail?.recommendations?.results.length
+                ),
+                CardNormal,
+                'movie'
+              )}
+            </section>
           </section>
-        </section>
         </div>
       ) : (
         <Loading />
